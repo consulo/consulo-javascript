9ab215738d6ee724a324a3e6c4ecac92d53621a6
VISTALL <vistall.valeriy@gmail.com>
2013-12-02 21:58:24 +0200

    some merge from old plugin

--- src/com/intellij/lang/javascript/JSONLexer.java
+++ src/com/intellij/lang/javascript/JSONLexer.java
@@ -4,16 +4,15 @@
 package com.intellij.lang.javascript;
 
 import org.jetbrains.annotations.Nullable;
+import com.intellij.lexer.DelegateLexer;
 import com.intellij.lexer.Lexer;
-import com.intellij.lexer.LexerBase;
 import com.intellij.psi.StringEscapesTokenTypes;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
 
-public final class JSONLexer extends LexerBase
+public final class JSONLexer extends DelegateLexer
 {
-	private final Lexer myLexer;
-	private final TokenSet myAllowedTokenSet = TokenSet.orSet(JSTokenTypes.IDENTIFIER_TOKENS_SET, TokenSet.create(JSTokenTypes.NULL_KEYWORD,
+	private static final TokenSet purAllowedTokenSet = TokenSet.orSet(JSTokenTypes.IDENTIFIER_TOKENS_SET, TokenSet.create(JSTokenTypes.NULL_KEYWORD,
 			JSTokenTypes.LBRACE, JSTokenTypes.LBRACKET, JSTokenTypes.RBRACE, JSTokenTypes.RBRACKET, JSTokenTypes.TRUE_KEYWORD,
 			JSTokenTypes.C_STYLE_COMMENT, JSTokenTypes.END_OF_LINE_COMMENT, JSTokenTypes.FALSE_KEYWORD, JSTokenTypes.NUMERIC_LITERAL,
 			JSTokenTypes.STRING_LITERAL, JSTokenTypes.SINGLE_QUOTE_STRING_LITERAL, JSTokenTypes.COMMA, JSTokenTypes.COLON, JSTokenTypes.WHITE_SPACE,
@@ -22,53 +21,18 @@ public final class JSONLexer extends LexerBase
 
 	public JSONLexer(Lexer baseLexer)
 	{
-		myLexer = baseLexer;
+		super(baseLexer);
 	}
 
-	public void start(final CharSequence buffer, final int startOffset, final int endOffset, final int initialState)
-	{
-		myLexer.start(buffer, startOffset, endOffset, initialState);
-	}
-
-	public int getState()
-	{
-		return myLexer.getState();
-	}
-
+	@Override
 	@Nullable
 	public IElementType getTokenType()
 	{
-		final IElementType tokenType = myLexer.getTokenType();
-		if(myAllowedTokenSet.contains(tokenType) || tokenType == null)
+		final IElementType tokenType = super.getTokenType();
+		if(purAllowedTokenSet.contains(tokenType) || tokenType == null)
 		{
 			return tokenType;
 		}
 		return JSTokenTypes.BAD_CHARACTER;
-	}
-
-	public int getTokenStart()
-	{
-		return myLexer.getTokenStart();
-	}
-
-	public int getTokenEnd()
-	{
-		return myLexer.getTokenEnd();
-	}
-
-	public void advance()
-	{
-		myLexer.advance();
-	}
-
-	@Override
-	public CharSequence getBufferSequence()
-	{
-		return myLexer.getBufferSequence();
-	}
-
-	public int getBufferEnd()
-	{
-		return myLexer.getBufferEnd();
 	}
 }
