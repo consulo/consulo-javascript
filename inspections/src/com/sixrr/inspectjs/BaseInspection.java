1412b8b1ee77f7291882be5b5e70041028835d1e
VISTALL <vistall.valeriy@gmail.com>
2015-04-13 17:13:41 +0300

    reformat

--- inspections/src/com/sixrr/inspectjs/BaseInspection.java
+++ inspections/src/com/sixrr/inspectjs/BaseInspection.java
@@ -19,84 +19,98 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiNamedElement;
 import com.intellij.psi.util.PsiTreeUtil;
 
-public abstract class BaseInspection extends LocalInspectionTool implements CustomSuppressableInspectionTool {
-    private final String m_shortName = null;
+public abstract class BaseInspection extends LocalInspectionTool implements CustomSuppressableInspectionTool
+{
+	private final String m_shortName = null;
 
-    @Override
+	@Override
 	@NotNull
-    public String getShortName() {
-        if (m_shortName == null) {
-            final Class<? extends BaseInspection> aClass = getClass();
-            @NonNls final String name = aClass.getName();
-            assert name.endsWith("Inspection") : "class name must end with the 'Inspection' to correctly calculate the short name: "+name;
-            return name.substring(name.lastIndexOf((int) '.') + 1,
-                    name.length() - "Inspection".length());
-        }
-        return m_shortName;
-    }
+	public String getShortName()
+	{
+		if(m_shortName == null)
+		{
+			final Class<? extends BaseInspection> aClass = getClass();
+			@NonNls final String name = aClass.getName();
+			assert name.endsWith("Inspection") : "class name must end with the 'Inspection' to correctly calculate the short name: " + name;
+			return name.substring(name.lastIndexOf((int) '.') + 1, name.length() - "Inspection".length());
+		}
+		return m_shortName;
+	}
 
-  @Override
-  @NotNull
-    public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder problemsHolder, boolean onTheFly){
-        final BaseInspectionVisitor visitor = buildVisitor();
-        visitor.setProblemsHolder(problemsHolder);
-        visitor.setOnTheFly(onTheFly);
-        visitor.setInspection(this);
-        return visitor;
-    }
+	@Override
+	@NotNull
+	public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder problemsHolder, boolean onTheFly)
+	{
+		final BaseInspectionVisitor visitor = buildVisitor();
+		visitor.setProblemsHolder(problemsHolder);
+		visitor.setOnTheFly(onTheFly);
+		visitor.setInspection(this);
+		return visitor;
+	}
 
 
-    @Nullable
-    protected String buildErrorString(Object... args) {
-        return null;
-    }
+	@Nullable
+	protected String buildErrorString(Object... args)
+	{
+		return null;
+	}
 
-    protected boolean buildQuickFixesOnlyForOnTheFlyErrors() {
-        return false;
-    }
+	protected boolean buildQuickFixesOnlyForOnTheFlyErrors()
+	{
+		return false;
+	}
 
-    @Nullable
-    protected InspectionJSFix buildFix(PsiElement location) {
-        return null;
-    }
+	@Nullable
+	protected InspectionJSFix buildFix(PsiElement location)
+	{
+		return null;
+	}
 
-    @Nullable
-    protected InspectionJSFix[] buildFixes(PsiElement location) {
-        return null;
-    }
+	@Nullable
+	protected InspectionJSFix[] buildFixes(PsiElement location)
+	{
+		return null;
+	}
 
-    public boolean hasQuickFix() {
-        final Method[] methods = getClass().getDeclaredMethods();
-        for (final Method method : methods) {
-            @NonNls final String methodName = method.getName();
-            if ("buildFix".equals(methodName)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public boolean hasQuickFix()
+	{
+		final Method[] methods = getClass().getDeclaredMethods();
+		for(final Method method : methods)
+		{
+			@NonNls final String methodName = method.getName();
+			if("buildFix".equals(methodName))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public abstract BaseInspectionVisitor buildVisitor();
+	public abstract BaseInspectionVisitor buildVisitor();
 
-    protected boolean functionHasIdentifier(JSFunction function) {
-        final ASTNode identifier = function.findNameIdentifier();
-        return identifier != null && PsiTreeUtil.isAncestor(function, identifier.getPsi(), true);
-    }
+	protected boolean functionHasIdentifier(JSFunction function)
+	{
+		final ASTNode identifier = function.findNameIdentifier();
+		return identifier != null && PsiTreeUtil.isAncestor(function, identifier.getPsi(), true);
+	}
 
-    @Override
-    public PsiNamedElement getProblemElement(final PsiElement psiElement) {
-        return PsiTreeUtil.getNonStrictParentOfType(psiElement, PsiFile.class);
-    }
+	@Override
+	public PsiNamedElement getProblemElement(final PsiElement psiElement)
+	{
+		return PsiTreeUtil.getNonStrictParentOfType(psiElement, PsiFile.class);
+	}
 
-    @Override
-	public SuppressIntentionAction[] getSuppressActions(@Nullable final PsiElement element) {
-        return new SuppressIntentionAction[] {
-           /* new AddNoInspectionCommentFix(HighlightDisplayKey.find(getShortName()), JSSuppressionHolder.class),  */
-        };
-    }
+	@Override
+	public SuppressIntentionAction[] getSuppressActions(@Nullable final PsiElement element)
+	{
+		return new SuppressIntentionAction[]{
+		   /* new AddNoInspectionCommentFix(HighlightDisplayKey.find(getShortName()), JSSuppressionHolder.class),  */
+		};
+	}
 
-    @Override
-	public boolean isSuppressedFor(final PsiElement element) {
-        return SuppressionUtil.isSuppressedInStatement(element, getID(), JSSuppressionHolder.class);
-    }
+	@Override
+	public boolean isSuppressedFor(@NotNull final PsiElement element)
+	{
+		return SuppressionUtil.isSuppressedInStatement(element, getID(), JSSuppressionHolder.class);
+	}
 }
--- inspections/src/com/sixrr/inspectjs/validity/ReservedWordUsedAsNameJSInspection.java
+++ inspections/src/com/sixrr/inspectjs/validity/ReservedWordUsedAsNameJSInspection.java
@@ -1,160 +1,183 @@
 package com.sixrr.inspectjs.validity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.intellij.lang.annotations.Pattern;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import com.intellij.lang.javascript.psi.JSProperty;
 import com.intellij.lang.javascript.psi.JSReferenceExpression;
 import com.intellij.lang.javascript.psi.util.JSUtils;
 import com.intellij.psi.PsiElement;
-import com.sixrr.inspectjs.*;
+import com.sixrr.inspectjs.BaseInspectionVisitor;
+import com.sixrr.inspectjs.InspectionJSBundle;
+import com.sixrr.inspectjs.InspectionJSFix;
+import com.sixrr.inspectjs.JSGroupNames;
+import com.sixrr.inspectjs.JavaScriptInspection;
 import com.sixrr.inspectjs.fix.RenameFix;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.HashSet;
-import java.util.Set;
+public class ReservedWordUsedAsNameJSInspection extends JavaScriptInspection
+{
+	@NonNls
+	private static Set<String> reservedWords = new HashSet<String>();
+	private static Set<String> reservedWordExclusionsForECMAScript = new HashSet<String>();
 
-public class ReservedWordUsedAsNameJSInspection extends JavaScriptInspection {
-    @NonNls
-    private static Set<String> reservedWords = new HashSet<String>();
-    private static Set<String> reservedWordExclusionsForECMAScript = new HashSet<String>();
+	static
+	{
+		reservedWords.add("abstract");
+		reservedWords.add("boolean");
+		reservedWords.add("break");
+		reservedWords.add("byte");
+		reservedWords.add("case");
+		reservedWords.add("catch");
+		reservedWords.add("char");
+		reservedWords.add("class");
+		reservedWords.add("continue");
+		reservedWords.add("const");
+		reservedWords.add("debugger");
+		reservedWords.add("default");
+		reservedWords.add("delete");
+		reservedWords.add("do");
+		reservedWords.add("double");
+		reservedWords.add("else");
+		reservedWords.add("extends");
+		reservedWords.add("enum");
+		reservedWords.add("export");
+		reservedWords.add("false");
+		reservedWords.add("final");
+		reservedWords.add("finally");
+		reservedWords.add("float");
+		reservedWords.add("for");
+		reservedWords.add("function");
+		reservedWords.add("if");
+		reservedWords.add("implements");
+		reservedWords.add("import");
+		reservedWords.add("in");
+		reservedWords.add("instanceof");
+		reservedWords.add("int");
+		reservedWords.add("interface");
+		//reservedWords.add("label"); // not a reserved word
+		reservedWords.add("long");
+		reservedWords.add("native");
+		reservedWords.add("new");
+		reservedWords.add("null");
+		reservedWords.add("package");
+		reservedWords.add("private");
+		reservedWords.add("protected");
+		reservedWords.add("public");
+		reservedWords.add("return");
+		reservedWords.add("short");
+		reservedWords.add("static");
+		reservedWords.add("super");
+		reservedWords.add("switch");
+		reservedWords.add("synchronized");
+		reservedWords.add("this");
+		reservedWords.add("throw");
+		reservedWords.add("throws");
+		reservedWords.add("transient");
+		reservedWords.add("true");
+		reservedWords.add("try");
+		reservedWords.add("typeof");
+		reservedWords.add("var");
+		reservedWords.add("void");
+		reservedWords.add("volatile");
+		reservedWords.add("while");
+		reservedWords.add("with");
 
-    static
-    {
-        reservedWords.add("abstract");
-        reservedWords.add("boolean");
-        reservedWords.add("break");
-        reservedWords.add("byte");
-        reservedWords.add("case");
-        reservedWords.add("catch");
-        reservedWords.add("char");
-        reservedWords.add("class");
-        reservedWords.add("continue");
-        reservedWords.add("const");
-        reservedWords.add("debugger");
-        reservedWords.add("default");
-        reservedWords.add("delete");
-        reservedWords.add("do");
-        reservedWords.add("double");
-        reservedWords.add("else");
-        reservedWords.add("extends");
-        reservedWords.add("enum");
-        reservedWords.add("export");
-        reservedWords.add("false");
-        reservedWords.add("final");
-        reservedWords.add("finally");
-        reservedWords.add("float");
-        reservedWords.add("for");
-        reservedWords.add("function");
-        reservedWords.add("if");
-        reservedWords.add("implements");
-        reservedWords.add("import");
-        reservedWords.add("in");
-        reservedWords.add("instanceof");
-        reservedWords.add("int");
-        reservedWords.add("interface");
-        //reservedWords.add("label"); // not a reserved word
-        reservedWords.add("long");
-        reservedWords.add("native");
-        reservedWords.add("new");
-        reservedWords.add("null");
-        reservedWords.add("package");
-        reservedWords.add("private");
-        reservedWords.add("protected");
-        reservedWords.add("public");
-        reservedWords.add("return");
-        reservedWords.add("short");
-        reservedWords.add("static");
-        reservedWords.add("super");
-        reservedWords.add("switch");
-        reservedWords.add("synchronized");
-        reservedWords.add("this");
-        reservedWords.add("throw");
-        reservedWords.add("throws");
-        reservedWords.add("transient");
-        reservedWords.add("true");
-        reservedWords.add("try");
-        reservedWords.add("typeof");
-        reservedWords.add("var");
-        reservedWords.add("void");
-        reservedWords.add("volatile");
-        reservedWords.add("while");
-        reservedWords.add("with");
+		reservedWordExclusionsForECMAScript.add("int");
+	}
 
-        reservedWordExclusionsForECMAScript.add("int");
-    }
-    @Override
+	@Pattern(value = "[a-zA-Z_0-9.-]+")
+	@Override
 	@NotNull
-    public String getID() {
-        return "ReservedWordAsName";
-    }
+	public String getID()
+	{
+		return "ReservedWordAsName";
+	}
 
-    @Override
+	@Override
 	@NotNull
-    public String getDisplayName() {
-        return InspectionJSBundle.message("reserved.word.used.as.name.display.name");
-    }
+	public String getDisplayName()
+	{
+		return InspectionJSBundle.message("reserved.word.used.as.name.display.name");
+	}
 
-    @Override
+	@Override
 	@NotNull
-    public String getGroupDisplayName() {
-        return JSGroupNames.VALIDITY_GROUP_NAME;
-    }
+	public String getGroupDisplayName()
+	{
+		return JSGroupNames.VALIDITY_GROUP_NAME;
+	}
 
-    @Override
+	@Override
 	@NotNull
-    public String buildErrorString(Object... args) {
-        return InspectionJSBundle.message("reserved.word.used.as.name.error.string");
-    }
+	public String buildErrorString(Object... args)
+	{
+		return InspectionJSBundle.message("reserved.word.used.as.name.error.string");
+	}
 
-    @Override
-	protected InspectionJSFix buildFix(PsiElement location) {
-        return new RenameFix();
-    }
+	@Override
+	protected InspectionJSFix buildFix(PsiElement location)
+	{
+		return new RenameFix();
+	}
 
-    @Override
-	protected boolean buildQuickFixesOnlyForOnTheFlyErrors() {
-        return true;
-    }
+	@Override
+	protected boolean buildQuickFixesOnlyForOnTheFlyErrors()
+	{
+		return true;
+	}
 
-    @Override
-	public boolean isEnabledByDefault() {
-        return  true;
-    }
+	@Override
+	public boolean isEnabledByDefault()
+	{
+		return true;
+	}
 
-    @Override
-	public BaseInspectionVisitor buildVisitor() {
-        return new ReservedWordAsNameVisitor();
-    }
+	@Override
+	public BaseInspectionVisitor buildVisitor()
+	{
+		return new ReservedWordAsNameVisitor();
+	}
 
-    private static class ReservedWordAsNameVisitor extends BaseInspectionVisitor {
+	private static class ReservedWordAsNameVisitor extends BaseInspectionVisitor
+	{
 
-        @Override public void visitJSProperty(JSProperty jsProperty) {
-            super.visitJSProperty(jsProperty);
-            final String name = jsProperty.getName();
-            if(isReserved(name))
-            {
-                registerError(jsProperty.getFirstChild());
-            }
-        }
+		@Override
+		public void visitJSProperty(JSProperty jsProperty)
+		{
+			super.visitJSProperty(jsProperty);
+			final String name = jsProperty.getName();
+			if(isReserved(name))
+			{
+				registerError(jsProperty.getFirstChild());
+			}
+		}
 
-        @Override public void visitJSReferenceExpression(JSReferenceExpression jsReferenceExpression) {
-            super.visitJSReferenceExpression(jsReferenceExpression);
-            final PsiElement nameElement = jsReferenceExpression.getReferenceNameElement();
-            if(nameElement== null)
-            {
-                return;
-            }
+		@Override
+		public void visitJSReferenceExpression(JSReferenceExpression jsReferenceExpression)
+		{
+			super.visitJSReferenceExpression(jsReferenceExpression);
+			final PsiElement nameElement = jsReferenceExpression.getReferenceNameElement();
+			if(nameElement == null)
+			{
+				return;
+			}
 
-            final String s = nameElement.getText();
-            if(isReserved(s)) {
-                if (!reservedWordExclusionsForECMAScript.contains(s) ||
-                    JSUtils.getDialect(jsReferenceExpression.getContainingFile()) == null) {
-                    registerError(nameElement);
-                }
-            }
-        }
-    }
+			final String s = nameElement.getText();
+			if(isReserved(s))
+			{
+				if(!reservedWordExclusionsForECMAScript.contains(s) || JSUtils.getDialect(jsReferenceExpression.getContainingFile()) == null)
+				{
+					registerError(nameElement);
+				}
+			}
+		}
+	}
 
-    private static boolean isReserved(String text) {
-        return reservedWords.contains(text);
-    }
+	private static boolean isReserved(String text)
+	{
+		return reservedWords.contains(text);
+	}
 }
