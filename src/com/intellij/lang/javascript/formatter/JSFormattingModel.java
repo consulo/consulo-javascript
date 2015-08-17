5c31426bdae097665b87e6ad51820317825058bd
VISTALL <vistall.valeriy@gmail.com>
2015-08-18 00:56:46 +0300

    compilation

--- src/com/intellij/lang/javascript/formatter/JSFormattingModel.java
+++ src/com/intellij/lang/javascript/formatter/JSFormattingModel.java
@@ -21,6 +21,7 @@ import com.intellij.formatting.Block;
 import com.intellij.formatting.FormattingDocumentModel;
 import com.intellij.formatting.FormattingModel;
 import com.intellij.formatting.FormattingModelProvider;
+import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
@@ -58,9 +59,9 @@ public class JSFormattingModel implements FormattingModel
 	}
 
 	@Override
-	public TextRange shiftIndentInsideRange(TextRange range, int indent)
+	public TextRange shiftIndentInsideRange(ASTNode node, TextRange range, int indent)
 	{
-		return myModel.shiftIndentInsideRange(range, indent);
+		return myModel.shiftIndentInsideRange(node, range, indent);
 	}
 
 	@Override
