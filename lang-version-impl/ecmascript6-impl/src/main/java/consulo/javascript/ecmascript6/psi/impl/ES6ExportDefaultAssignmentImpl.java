7a7d0ce49b4610d8606bef391de32f5f4f7756bc
VISTALL <vistall.valeriy@gmail.com>
2020-01-10 14:04:37 +0300

    allow search class from export default

--- lang-version-impl/ecmascript6-impl/src/main/java/consulo/javascript/ecmascript6/psi/impl/ES6ExportDefaultAssignmentImpl.java
+++ lang-version-impl/ecmascript6-impl/src/main/java/consulo/javascript/ecmascript6/psi/impl/ES6ExportDefaultAssignmentImpl.java
@@ -1,8 +1,16 @@
 package consulo.javascript.ecmascript6.psi.impl;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.lang.javascript.psi.JSClass;
+import com.intellij.lang.javascript.psi.JSElement;
 import com.intellij.lang.javascript.psi.impl.JSStatementImpl;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.ResolveState;
+import com.intellij.psi.scope.PsiScopeProcessor;
 import consulo.javascript.ecmascript6.psi.ES6ExportDefaultAssignment;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * @author VISTALL
@@ -13,5 +21,22 @@ public class ES6ExportDefaultAssignmentImpl extends JSStatementImpl implements E
 	public ES6ExportDefaultAssignmentImpl(ASTNode node)
 	{
 		super(node);
+	}
+
+	@Nullable
+	public JSElement getElement()
+	{
+		return findChildByClass(JSClass.class);
+	}
+
+	@Override
+	public boolean processDeclarations(@Nonnull PsiScopeProcessor processor, @Nonnull ResolveState state, PsiElement lastParent, @Nonnull PsiElement place)
+	{
+		JSElement element = getElement();
+		if(element != null && !element.processDeclarations(processor, state, lastParent, place))
+		{
+			return false;
+		}
+		return super.processDeclarations(processor, state, lastParent, place);
 	}
 }
