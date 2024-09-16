45891aa43502cf8f0d20a39dcbaa0897534484bd
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-09-16 19:17:46 +0400

    context fix

--- plugin/src/main/java/com/intellij/lang/javascript/impl/JavaScriptCodeContextType.java
+++ plugin/src/main/java/com/intellij/lang/javascript/impl/JavaScriptCodeContextType.java
@@ -19,83 +19,72 @@ package com.intellij.lang.javascript.impl;
 import com.intellij.lang.javascript.JavaScriptSupportLoader;
 import com.intellij.lang.javascript.psi.JSFile;
 import com.intellij.lang.javascript.psi.resolve.JSResolveUtil;
+import consulo.annotation.access.RequiredReadAction;
 import consulo.annotation.component.ExtensionImpl;
-import consulo.javascript.language.JavaScriptFileType;
 import consulo.javascript.language.JavaScriptLanguage;
 import consulo.javascript.localize.JavaScriptLocalize;
 import consulo.language.Language;
 import consulo.language.editor.highlight.SyntaxHighlighter;
 import consulo.language.editor.highlight.SyntaxHighlighterFactory;
-import consulo.language.editor.template.context.TemplateContextType;
+import consulo.language.editor.template.context.BaseTemplateContextType;
+import consulo.language.editor.template.context.TemplateActionContext;
 import consulo.language.inject.InjectedLanguageManager;
 import consulo.language.psi.PsiElement;
 import consulo.language.psi.PsiFile;
 import consulo.language.psi.PsiLanguageInjectionHost;
 import consulo.language.psi.util.PsiTreeUtil;
 import consulo.util.lang.ref.Ref;
-import consulo.virtualFileSystem.fileType.FileType;
 import consulo.xml.lang.xml.XMLLanguage;
 import jakarta.annotation.Nonnull;
 
 /**
  * @author Maxim.Mossienko
- *         Date: Apr 14, 2008
- *         Time: 6:01:09 PM
+ * Date: Apr 14, 2008
+ * Time: 6:01:09 PM
  */
 @ExtensionImpl
-public class JavaScriptCodeContextType extends TemplateContextType
-{
-	private static final String JAVA_SCRIPT = "JAVA_SCRIPT";
+public class JavaScriptCodeContextType extends BaseTemplateContextType {
+    private static final String JAVA_SCRIPT = "JAVA_SCRIPT";
 
-	public JavaScriptCodeContextType()
-	{
-		super(JAVA_SCRIPT, JavaScriptLocalize.javascriptTemplateContextType().get());
-	}
+    public JavaScriptCodeContextType() {
+        super(JAVA_SCRIPT, JavaScriptLocalize.javascriptTemplateContextType());
+    }
 
-	@Override
-	public boolean isInContext(@Nonnull final PsiFile file, final int offset)
-	{
-		PsiElement at = file.findElementAt(offset);
-		if(at == null && offset == file.getTextLength())
-		{
-			at = file.findElementAt(offset - 1);
-		}
-		Language language = at != null ? at.getParent().getLanguage() : null;
+    @Override
+    @RequiredReadAction
+    public boolean isInContext(@Nonnull TemplateActionContext context) {
+        PsiFile file = context.getFile();
+        int offset = context.getStartOffset();
+        
+        PsiElement at = file.findElementAt(offset);
+        if (at == null && offset == file.getTextLength()) {
+            at = file.findElementAt(offset - 1);
+        }
+        Language language = at != null ? at.getParent().getLanguage() : null;
 
-		if(language instanceof XMLLanguage)
-		{
-			final PsiLanguageInjectionHost host = PsiTreeUtil.getParentOfType(at, PsiLanguageInjectionHost.class, false);
+        if (language instanceof XMLLanguage) {
+            final PsiLanguageInjectionHost host = PsiTreeUtil.getParentOfType(at, PsiLanguageInjectionHost.class, false);
 
-			if(host != null)
-			{
-				final Ref<Boolean> hasJsInjection = new Ref<Boolean>(Boolean.FALSE);
+            if (host != null) {
+                final Ref<Boolean> hasJsInjection = new Ref<>(Boolean.FALSE);
 
-				InjectedLanguageManager.getInstance(at.getProject()).enumerate(host, new JSResolveUtil.JSInjectedFilesVisitor()
-				{
-					@Override
-					protected void process(final JSFile file)
-					{
-						hasJsInjection.set(Boolean.TRUE);
-					}
-				});
+                InjectedLanguageManager.getInstance(at.getProject()).enumerate(host, new JSResolveUtil.JSInjectedFilesVisitor() {
+                    @Override
+                    protected void process(final JSFile file) {
+                        hasJsInjection.set(Boolean.TRUE);
+                    }
+                });
 
-				if(hasJsInjection.get())
-				{
-					language = JavaScriptLanguage.INSTANCE;
-				}
-			}
-		}
-		return language != null && language.isKindOf(JavaScriptLanguage.INSTANCE);
-	}
+                if (hasJsInjection.get()) {
+                    language = JavaScriptLanguage.INSTANCE;
+                }
+            }
+        }
+        return language != null && language.isKindOf(JavaScriptLanguage.INSTANCE);
+    }
 
-	public boolean isInContext(@Nonnull final FileType fileType)
-	{
-		return fileType instanceof JavaScriptFileType;
-	}
-
-	@Override
-	public SyntaxHighlighter createHighlighter()
-	{
-		return SyntaxHighlighterFactory.getSyntaxHighlighter(JavaScriptSupportLoader.ECMA_SCRIPT_L4, null, null);
-	}
+    @Override
+    public SyntaxHighlighter createHighlighter() {
+        return SyntaxHighlighterFactory.getSyntaxHighlighter(JavaScriptSupportLoader.ECMA_SCRIPT_L4, null, null);
+    }
 }
